59604aacd97634a9b834d301ae6d932696ce2aeb
Biggs <biggsy@pm.me>
2025-10-28 11:52:29 +0000

    new colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lapis-lazuli': '#386595',
+        'night': '#121716',
+        'zomp': '#319876',
+        'gunmetal': '#252C2B',
+        'eerie-back': '#171C1C',
+        'viridian': '#287B5F',
+        'violet-blue': '#4A429E',
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
