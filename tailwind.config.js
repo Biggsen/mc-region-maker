df5cf02671921bff0f1002f4cfb579c83f36bfbc
Biggs <biggsy@pm.me>
2025-10-28 13:41:29 +0000

    few more colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,14 @@ export default {
         'lapis-lazuli': '#386595', // primary
         'viridian': '#287B5F', // secondary
         'violet-blue': '#4A429E',
+        'silver-lake-blue': '#5C8DC1',
+        'vista-blue': '#7AA2CD',
         'night': '#121716',
         'zomp': '#319876',
         'gunmetal': '#252C2B',
         'eerie-back': '#171C1C',
+        'hover-surface': '#2D3433', // ghost button hover
+        'active-surface': '#1E2322', // ghost button active
       },
     },
   },
